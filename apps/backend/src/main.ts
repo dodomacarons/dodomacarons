f6f8f8a271bfa5a279a69152d91d76b50341ba08
dodomacarons <dodomacarons@gmail.com>
2025-04-10 15:14:34 +0200

    add backend

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const host = '0.0.0.0';
+const port = 4200;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
